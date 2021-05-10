0bebd701b9f5ca7db6a1838d6313e2a58ae725f3
x39js <74694398+x39js@users.noreply.github.com>
2021-05-09 18:43:04 -0600

    Update pkgs.sh

--- pkgs.sh
+++ pkgs.sh
@@ -7,7 +7,3 @@ sudo apt upgrade -y
 sudo apt install git curl wget sudo npm nodejs openjdk-11-jdk openjdk-11-jre -y
 
 clear
-
-#Pull down my MultiMC Profiles
-cd ~/.local/share/multimc/instances
-git clone https://github.com/x39js/multimc-profiles.git
